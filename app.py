52724feb520e86351893c7fa5cca88014c4dad0c
Nattchi <48373518+Nattchi@users.noreply.github.com>
2022-08-02 17:34:00 +0900

    slackAPI_init

---
+++ app.py
@@ -0,0 +1,71 @@
+import logging
+import os
+
+from slack_bolt import App
+
+from slack_sdk.web import WebClient
+
+import os
+from slack_bolt import App
+
+# Initialize a Bolt for Python app
+token = os.environ.get("SLACK_BOT_TOKEN")
+secret = os.environ.get("SLACK_SIGNING_SECRET")
+
+channel_id = "C03RXBYEVQA"
+
+client = WebClient(token=token)
+
+# Initializes your Bolt app with a bot token and signing secret
+app = App(
+    token=token,
+    signing_secret=secret
+)
+
+
+def send_post_message():
+    response = client.api_call(
+        api_method='chat.postMessage',
+        json={'channel': channel_id, 'text': "Hello world!"}
+    )
+    assert response["ok"]
+    assert response["message"]["text"] == "Hello world!"
+
+    prev_message = response['message']['text']
+    response = client.conversations_history(channel=channel_id)
+    print(response['messages'])
+
+
+# Listens to incoming messages that contain "hello"
+# To learn available listener method arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click",
+                },
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!",
+    )
+
+
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked the button")
+
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
+
