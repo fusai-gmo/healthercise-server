ddf561e9d5d0e47f0d70e5c75551ffdb23f58690
haru911-99 <haruki08070743@gmail.com>
2022-08-03 02:05:07 +0900

    [add] migration file

---
+++ python/app/access_token.py
@@ -0,0 +1,26 @@
+import sys
+import user
+import token_type
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from setting import Base
+from setting import ENGINE
+
+class access_token(Base):
+    """
+    access_tokenモデル
+    """
+    __tablename__ = 'access_token'
+    id = Column('id', Integer, primary_key = True)
+    user_id = Column('user_id',ForeignKey('user.id'))
+    token_type_id = Column('token_type_id', ForeignKey('token_type.id'))
+    token = Column('token', String(300))
+
+def main(args):
+    """
+    メイン関数
+    """
+    Base.metadata.create_all(bind=ENGINE, checkfirst=True)
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ python/app/activity_level.py
@@ -0,0 +1,26 @@
+import sys
+import user
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from setting import Base
+from setting import ENGINE
+
+class activity_level(Base):
+    """
+    activity_levelモデル
+    """
+    __tablename__ = 'activity_level'
+    id = Column('id', Integer, primary_key = True)
+    user_id = Column('user_id', ForeignKey("user.id"))
+    level = Column('level', String(300))
+
+
+def main(args):
+    """
+    メイン関数
+    """
+    Base.metadata.create_all(bind=ENGINE, checkfirst=True)
+
+if __name__ == "__main__":
+    main(sys.argv)
