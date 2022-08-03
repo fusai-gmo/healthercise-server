2a65dd7e01f8a7241a3fb447158652ac1ccb587c
haru911-99 <haruki08070743@gmail.com>
2022-08-03 10:58:48 +0900

    [add] migration script

---
+++ python/app/migration.sh
@@ -0,0 +1,15 @@
+
+pip install sqlalchemy
+pip install sqlalchemy-migrate
+pip install pymysql
+pip install mysqlclient
+
+
+python user.py
+python sex.py
+python commute.py
+python activity_level.py
+python token_type.py
+python access_token.py
+python activity_log.py
+python activity_summary.py
