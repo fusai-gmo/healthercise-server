e4a8bc3268ecf126e74c4fc591e9a08c2e74e6ec
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-08-04 18:33:24 +0900

    apply validation

--- api/app/routers/user.py
+++ api/app/routers/user.py
@@ -3,11 +3,14 @@ from fastapi import APIRouter, HTTPException, Cookie, Header
 import schemas.user as user_schema
 import cruds.user as user_cruds
 from setting import session as db
+from auth.id_token import verify_id_token
 
 router = APIRouter()
 
 @router.post('/user')
 async def add_new_user(user: user_schema.UserCreate, id_token: Optional[str] = Cookie(None)):
+    verify_id_token(id_token)
+
     db_user = user_cruds.get_user_by_email(db, email=user.email)
     # Emailが登録済みの場合はエラーを返す
     if db_user is not None:
