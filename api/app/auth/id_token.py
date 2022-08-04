dc1cc3d94af492a1f5de5b7649997471f4593144
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-08-04 18:33:14 +0900

    add validation func

---
+++ api/app/auth/id_token.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+from google.oauth2 import id_token as google_id_token
+from google.auth.transport import requests as google_requests
+import os
+
+client_id = os.getenv("GOOGLE_API_CLIENT_ID", "not set")
+
+"""
+id_tokenが正当かチェックする
+"""
+def verify_id_token(id_token: str):
+  if id_token is None:
+      raise HTTPException(status_code=401, detail="Id token is not set")
+  id_info = google_id_token.verify_oauth2_token(id_token, google_requests.Request(), client_id)
+  print(id_info)
+  user_info = {
+    'uid': id_info['sub'],
+    'email': id_info['email'],
+  }
+  return user_info
+
+"""
+ユーザー本人によるリクエストかチェックし、(firebaseの) user_idを返す
+"""
+def verify_user(id_token: str, user_id: str):
+  user_info = verify_id_token(id_token)
+  if user_info['uid'] != user_id:
+    raise HTTPException(status_code=403, detail="Forbidden")
+  return user_info
+
