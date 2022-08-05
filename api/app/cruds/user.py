8b04e938ee082f5d0965db2832bed9eff7692764
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-08-05 13:03:49 +0900

    change save_refresh_token

--- api/app/cruds/user.py
+++ api/app/cruds/user.py
@@ -4,7 +4,7 @@ import models.sex as sex_model
 import models.commute as commute_model
 import models.activity_level as activity_level_model
 import schemas.user as user_schema
-import cruds.token as token_crud
+# import cruds.token as token_crud
 from sqlalchemy import Time
 from datetime import datetime as dt
 
@@ -128,3 +128,42 @@ async def create_user(db: Session, user: user_schema.UserCreate):
     db.commit()
 
     return db_user
+
+
+async def save_refresh_token(db: Session, access_token: str, firebase_id: str, user):
+  if user is None:
+    user_create = UserCreate(
+      userName=None,
+      firebaseId=firebase_id,
+      email=None,
+      gender="male",
+      age=20,
+      height=180,
+      weight=65,
+      activeLevel=1,
+      includeCommutingTime=False,
+      goWorkTime=TimeDuration(
+        start="09:00",
+        finish="10:00",
+      ),
+      leaveWorkTime=TimeDuration(
+        start="18:00",
+        finish="19:00",
+      ),
+      activeTime=TimeDuration(
+        start="10:00",
+        finish="19:00",
+      ),
+      slackId=None,
+    )
+    
+    res = await create_user(db, user_create)
+    
+    user_id = res.id
+
+  else:
+    user_id = user.id
+
+  query = sql_text("INSERT INTO access_token VALUES (:id, :user_id, :token_type_id, :token) ON DUPLICATE KEY UPDATE token = :token2")
+  db.execute(query, {"id": 0, "user_id": user_id, "token_type_id": 1, "token": access_token, "token2": access_token})
+  db.commit()
