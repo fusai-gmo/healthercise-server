14d2416b61aa77c8c8b48913a76dafdd10e2a7b4
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-08-05 10:58:16 +0900

    refresh tokenを保存

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -12,23 +12,24 @@ class user(Base):
     """
     __tablename__ = 'user'
     id = Column('id', Integer, primary_key = True, autoincrement=True)
-    firebase_id = Column('firebase_id', String(50), unique = True, nullable = False)
+    firebase_id = Column('firebase_id', String(50), unique = True)
     name = Column('name', String(200))
     email = Column('email', String(100))
     age = Column('age', Integer)
     height = Column('height', Integer)
     weight = Column('weight', Integer)
     slack_id = Column('slack_id', String(100))
+    notify_start_time = Column( 'notify_start_time', Time)
+    notify_finish_time = Column( 'notify_finish_time', Time)
+
     sex = relationship('sex')
     commute = relationship('commute')
     activity_level = relationship('activity_level')
-    access_token = relationship('access_token')
+    # access_token = relationship('access_token')
     activity_log = relationship('activity_log')
     activity_summary = relationship('activity_summary')
     
 
-    notify_start_time = Column( 'notify_start_time', Time)
-    notify_finish_time = Column( 'notify_finish_time', Time)
 
 # def main(args):
 #     """
